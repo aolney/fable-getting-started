76cc0db240950c6609dce408d92affcb7357ef16
Pauan <pcxunlimited@gmail.com>
2017-02-01 13:19:42 -1000

    Changing babel-plugin-transform-runtime to core-js

--- src/fs/Main.fs
+++ src/fs/Main.fs
@@ -3,6 +3,9 @@ module App.Main
 open Fable.Import
 open App.Message
 
+// This must be before everything else
+Fable.Core.JsInterop.importAll "core-js"
+
 let main () =
     Browser.console.log message
 
