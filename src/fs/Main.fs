cce5956b3a491da9c9dc67e08f4b8a8a2d860ca8
Pauan <pcxunlimited@gmail.com>
2017-02-01 14:31:42 -1000

    Changing to use babel-preset-env

--- src/fs/Main.fs
+++ src/fs/Main.fs
@@ -1,10 +1,10 @@
 module App.Main
 
+// This must be before everything else
+Fable.Core.JsInterop.importAll("babel-polyfill") |> ignore
+
 open Fable.Import
 open App.Message
-
-// This must be before everything else
-Fable.Core.JsInterop.importAll "core-js"
 
 let main () =
     Browser.console.log message
--- test/fs/Message.fs
+++ test/fs/Message.fs
@@ -4,6 +4,9 @@ open Fable.Core.Testing
 
 [<TestFixture>]
 module Message =
+    // This must be before everything else
+    Fable.Import.Node.require.Invoke("babel-polyfill") |> ignore
+
     [<Test>]
     let ``message should be correct`` () =
         App.Message.message |> equal "Hello world!"
